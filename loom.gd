7ef005af645682126df0472a7a4824a88391c65b
Aster <asterj.greenblatt@gmail.com>
2025-07-26 16:46:49 +1200

    refactor for magic numbers

--- loom.gd
+++ loom.gd
@@ -9,31 +9,54 @@ var layerCount = -1
 var threads = []
 const DEFAULT_LAYER = preload("res://thread_layer.tscn")
 
+var goalPattern = []
+var GridStates = []
+
+## LOOM FRAME CONSTANTS ##
+const FRAME_ROW = 1
+const FRAME_VERT_OFFSET = 1
+const PEG_ROW = 0
+const PEG_COL = 0
+
+
+## BUTTON LOCATIONS ##
 const mat1Pos = Vector2i(11, 4)
 const mat2Pos = Vector2i(11, 6)
+const mat3Pos = Vector2i(11, 8)
+const color1Pos = Vector2i(12, 4)
+const color2Pos = Vector2i(12, 6)
 
 const undoPos = Vector2i(15, 5)
+const checkmePos = Vector2i(15, 7)
+const toMapButton = Vector2i(17, 6)
 
-var GridStates = []
+## TILESET INDICES ##
+const EMPTY = 0
+const LOOM_FRAME = 1
+const COTTON = 2
+const LINEN = 3
+const WOOL = 4
+const LOOM_PEG = 5
+const BLUE = 6
+const RED = 7
+
+## TILESET VARIANT INDICES
+const VERTICAL = 0		# y index
+const HORIZONTAL = 1		# y index
+const NUM_VARIANTS = 0	# Can use this to generate random wtihin a range
 
 func initializeGridData() -> void:
 	for col in range(GridWidth):
 		LoomGrid.append([])
 		for row in range(GridHeight):
-			if ((row == 1 || row == GridHeight-1)):
-				LoomGrid[col].append(1)
-			elif ((row == 0 && col > 0) || ((row > 0 && row < GridHeight-1) && col == 0)):
-				LoomGrid[col].append(4)
+			if ((row == FRAME_ROW || row == GridHeight-FRAME_VERT_OFFSET)):
+				LoomGrid[col].append(LOOM_FRAME)
+			elif ((row == PEG_ROW && col > 0) || ((row > 0 && row < GridHeight-1) && col == PEG_COL)):
+				LoomGrid[col].append(LOOM_PEG)
 			else:
-				LoomGrid[col].append(0)
+				LoomGrid[col].append(EMPTY)
 	GridStates.append(LoomGrid.duplicate(true))
-
-#func storeMove() -> void:
-#	GridStates.append([])
-#	for col in range(GridWidth):
-#		GridStates[layerCount+1].append([])
-#		for row in range(GridHeight):
-			
+	goalPattern = LoomGrid.duplicate(true)
 
 func displayGridData() -> void:
 	for col in range(GridWidth):
@@ -42,28 +65,38 @@ func displayGridData() -> void:
 			set_cell(Vector2(col, row), LoomGrid[col][row], Vector2(0, 0), 0)
 
 
-func _ready() -> void:			
+func _ready() -> void:
 	initializeGridData()
 	displayGridData()
 	
-	set_cell(mat1Pos, 2, Vector2(0, 0), 0)
-	set_cell(mat2Pos, 3, Vector2(0, 0), 0)
+	set_cell(mat1Pos, COTTON, Vector2(0, 0), 0)
+	set_cell(mat2Pos, LINEN, Vector2(0, 0), 0)
+	set_cell(mat3Pos, WOOL, Vector2(0, 0), 0)
 	
-	set_cell(undoPos, 0, Vector2(0, 0), 0)
+	set_cell(color1Pos, BLUE, Vector2(0, 0), 0)
+	set_cell(color2Pos, RED, Vector2(0, 0), 0)
+	
+	set_cell(undoPos, EMPTY, Vector2(0, 0), 0)
+	set_cell(checkmePos, LOOM_PEG, Vector2(0, 0), 0)
+	set_cell(toMapButton, LOOM_PEG, Vector2(0, 0), 0)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	mouseTile = local_to_map(get_global_mouse_position())
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		if (mouseTile == mat1Pos || mouseTile == mat2Pos):
+		if (mouseTile == mat1Pos || mouseTile == mat2Pos || mouseTile == mat3Pos):
 			GrabResource()
 		elif (mouseTile == undoPos):
 			UndoMove()
+		elif (mouseTile == checkmePos):
+			CheckMe()
+		elif (mouseTile == toMapButton):
+			print("Going exploring...")
 		elif ((mouseTile.y == 0 && mouseTile.x == 0) ||
 		(mouseTile.x > GridWidth || mouseTile.y > GridHeight)):
 			pass
-		elif (mouseTile.y == 0 && (mouseValue in [2, 3])):	
+		elif (mouseTile.y == 0 && (mouseValue in range(2, 5))):	
 			layerCount += 1
 			var new_layer = DEFAULT_LAYER.instantiate()
 			add_child(new_layer)
@@ -100,3 +133,9 @@ func UndoMove() -> void:
 
 func GrabResource() -> void:
 	mouseValue = get_cell_source_id(mouseTile)
+
+func CheckMe() -> void:
+	if (LoomGrid == goalPattern):
+		set_cell(checkmePos, LOOM_FRAME, Vector2i(0, 0), 0)
+	
+	
