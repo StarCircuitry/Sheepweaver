ff9fd7dfc7603d11c36deeb65d075b5ab45fb432
s-mahnoor-q <105626403+s-mahnoor-q@users.noreply.github.com>
2025-07-27 12:27:50 +1200

    added scene manager classes
    not connected yet

---
+++ loom_station.gd
@@ -0,0 +1,6 @@
+## loom_station.gd
+extends Node
+
+func _on_loom_station_button_pressed():
+	print_debug("Return to menu triggered")
+	#Global.goto_scene("res://menu.tscn")
---
+++ shop_front.gd
@@ -0,0 +1,6 @@
+## shop_front.gd
+extends Node
+
+func _return_to_shop_pressed():
+	print_debug("Return to SHOP triggered")
+	#Global.goto_scene("res://shop.tscn")
